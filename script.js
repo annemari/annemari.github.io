6c58f856b892073b294854a7b7746783f2e125e6
Anne-Mari Eensaar <55237833+annemari@users.noreply.github.com>
2022-04-07 22:11:52 +0300

    portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+// MDB Lightbox Init
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+$('#myCollapsible').on('hidden.bs.collapse', function () {
+// do something…
+})
+
+//Typewriter
+
+// CHAR SHEET
